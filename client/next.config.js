077c3c29203e11c642125b25f6a006650d59df28
Arnab Hazra <arnhazra@gmail.com>
2024-01-14 15:14:07 +0530

    ProdDeploy

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    unoptimized: true
+  },
+  output: "export",
+  trailingSlash: process.env.NODE_ENV === "production" ? true : false
+}
 
 module.exports = nextConfig
