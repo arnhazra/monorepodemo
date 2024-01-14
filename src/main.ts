e95a32566942a74974bdea6a50e65887edf693f7
Arnab Hazra <arnhazra@gmail.com>
2024-01-14 15:53:18 +0530

    ProdDeploy

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import { envConfig } from "./config/env.config"
+import { INestApplication } from "@nestjs/common"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app: INestApplication<any> = await NestFactory.create(AppModule)
   app.setGlobalPrefix("api")
+  app.enableCors()
   await app.listen(envConfig.apiPort)
 }
 
