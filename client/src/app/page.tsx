d88b569120644bb7cc77f0fe94c5c93e3ddbffe6
Arnab Hazra <arnhazra@gmail.com>
2024-01-30 11:36:30 +0530

    publish

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,10 @@
 export default async function HomePage() {
   const response = await fetch(process.env.NODE_ENV === 'production' ? 'https://monorepodemo.vercel.app/api' : 'http://localhost:8000/api', { next: { revalidate: 10 } })
-  const data = await response.json()
+
 
   return (
     <main>
       <h1>
-        {data.message}
       </h1>
     </main>
   )
